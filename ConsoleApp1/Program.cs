d99b55a05a24c84d9c22cfb0835828ce8738545e
Kseniya Dubouskaya <dubovskaya.ksenia@gmail.com>
2023-12-20 21:52:57 +0300

    кнтр

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Task1
+{
+    int[] array = new int[10];
+    Random rnd = new Random();
+
+    int i = 0;
+    for (int i = 0; i < array.Length; i++)
+ {
+        array[i] = rnd.Next(20);
+ }
+
+public int secondPart = from n in array
+                        orderby n
+                        select n;
+    }
+    //не хватило времени из-за проблем с интернетом
+}
